59ac4b5bdeea28019ae9f354a86e95a6a9942ca0
Gabriel Pivaro <gabrconc@gmail.com>
2020-08-16 00:42:31 -0500

    PyBank Done

---
+++ PyBank/main.py
@@ -0,0 +1,85 @@
+import csv
+
+# Relative path for the csv file dataset
+file_path = "Resources/Homework_03-Python_Instructions_PyBank_Resources_budget_data.csv"
+output_file_path = "analysis/Financial_Analysis.txt"
+# Create the empty list for the dataset
+date = []
+Profit_Losses = []
+
+# Open and read the dataset file
+with open(file_path, "r") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    header = next(csvreader)
+
+    for row in csvreader:
+        date.append(row[0])
+        Profit_Losses.append(int(row[1]))
+
+
+# The total number of months included in the dataset
+total_number_months = len(date)
+
+# The net total amount of "Profit/Losses" over the entire period
+total_amount_Profit_Losses = sum(Profit_Losses)
+
+# The average of the changes in "Profit/Losses" over the entire period
+changes = []
+for i in range(len(Profit_Losses) - 1):
+    changes.append(Profit_Losses[i + 1] - Profit_Losses[i])
+average_changes_Profit_Losses = sum(changes) / (total_number_months - 1)
+
+# The greatest increase in profits (date and amount) over the entire period
+greatest_increase_profits = 0
+for i in range(len(Profit_Losses)):
+    if Profit_Losses[i] - Profit_Losses[i - 1] > greatest_increase_profits:
+        greatest_increase_profits = Profit_Losses[i] - Profit_Losses[i - 1]
+        date_greatest_increase_profits = date[i]
+
+
+# The greatest decrease in losses (date and amount) over the entire period
+greatest_decrease_losses = 0
+for i in range(len(Profit_Losses)):
+    if Profit_Losses[i] - Profit_Losses[i - 1] < greatest_decrease_losses:
+        greatest_decrease_losses = Profit_Losses[i] - Profit_Losses[i - 1]
+        date_greatest_decrease_losses = date[i]
+
+
+# Print the final results in the terminal
+print()
+print("Financial Analysis")
+print("----------------------------")
+print(f"Total Months: {total_number_months}")
+print(f"Total ${total_amount_Profit_Losses:,.0f}".replace("$-", "-$"))
+print(f"Average Change ${average_changes_Profit_Losses:,.0f}".replace("$-", "-$"))
+print(
+    f"Greatest Increase in Profits: {date_greatest_increase_profits} (${greatest_increase_profits:,.0f})".replace(
+        "$-", "-$"
+    )
+)
+print(
+    f"Greatest Decrease in Profits: {date_greatest_decrease_losses} (${greatest_decrease_losses:,.0f})".replace(
+        "$-", "-$"
+    )
+)
+
+
+with open(output_file_path, "w") as text:
+    text.write("Financial Analysis\n")
+    text.write("----------------------------\n")
+    text.write(f"Total Months: {total_number_months}\n")
+    text.write(f"Total ${total_amount_Profit_Losses:,.0f}\n".replace("$-", "-$"))
+    text.write(
+        f"Average Change ${average_changes_Profit_Losses:,.0f}\n".replace("$-", "-$")
+    )
+    text.write(
+        f"Greatest Increase in Profits: {date_greatest_increase_profits} (${greatest_increase_profits:,.0f})\n".replace(
+            "$-", "-$"
+        )
+    )
+    text.write(
+        f"Greatest Decrease in Profits: {date_greatest_decrease_losses} (${greatest_decrease_losses:,.0f})\n".replace(
+            "$-", "-$"
+        )
+    )
